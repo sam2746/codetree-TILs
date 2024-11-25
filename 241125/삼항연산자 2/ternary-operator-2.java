dc6c5ff6132098ba330a4af2871a5a3d459d1733
kmin <32429549+sam2746@users.noreply.github.com>
2024-11-25 19:24:44 +0900

    Title: 삼항연산자 2, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241125/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+
+       char result =  (a==1) ? 't' : 'f';
+
+       System.out.println(result);
+    }
+}
