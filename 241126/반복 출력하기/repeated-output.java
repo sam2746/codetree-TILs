9e01f0e3f3a28999042ae46773ee02462be35eb0
kmin <32429549+sam2746@users.noreply.github.com>
2024-11-26 18:11:11 +0900

    Title: 반복 출력하기, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241126/반복 출력하기/repeated-output.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        
+        printStar(n);
+
+
+    }
+
+    public static void printStar(int n){
+        for(int i = 0;i<n;i++){
+            System.out.println("12345^&*()_");
+            
+        }
+    }
+
+}
