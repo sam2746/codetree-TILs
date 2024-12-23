4a39d2ee761720be0e6523fd197d525c3695eb5b
kmin <32429549+sam2746@users.noreply.github.com>
2024-12-23 15:12:17 +0900

    Title: Time to Time, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241223/Time to Time/time-to-time.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        int min = 0;
+        min += 60*(c-a);
+        min += (d-b);
+
+        System.out.println(min);
+    }
+}
