fc3a26dc049d9eeff60f5e128e9fcf5b791802d2
kmin <32429549+sam2746@users.noreply.github.com>
2024-12-24 14:31:33 +0900

    Title: 블럭쌓는 명령2, Time: 131ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241224/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 입력부 만들기
+        // 리스트로 블럭 쌓인 현황 추상화
+        // 최대 블럭 수 계산 함수
+
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+
+        //scanner.nextLine();
+
+        int[] blockStack = new int[N];
+
+        for(int i = 1;i<=K;i++){
+            //반복해서 A, B 입력받고 list에 쌓기
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            //scanner.nextLine();
+
+            for(int k = A;k<=B;k++){
+                blockStack[k] ++;
+            }
+        }
+
+        int max = 0;
+
+        for(int i = 0;i<N;i++){
+            if(blockStack[i]>= max){
+                max = blockStack[i];
+            }
+        }
+
+        System.out.println(max);
+
+    }
+}
