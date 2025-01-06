5422d0bb5f0cd3f65670077f405f11148bb45c01
kmin <32429549+sam2746@users.noreply.github.com>
2025-01-06 13:15:27 +0900

    Title: 왔다 갔던 구역 2, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250106/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 250106/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -12,8 +12,18 @@ public class Main {
             int num = scanner.nextInt(); // 이동 거리
             char direction = scanner.next().charAt(0); // 이동 방향
 
-            // 경로 기록 및 현재 위치 업데이트
-            currentIdx = record(num, direction, arr, currentIdx);
+            // 이동 경로 기록
+            if (direction == 'L') {
+                for (int j = 1; j <= num; j++) {
+                    arr[currentIdx - j]++;
+                }
+                currentIdx -= num; // 최종 위치 업데이트
+            } else { // 'R'
+                for (int j = 1; j <= num; j++) {
+                    arr[currentIdx + j]++;
+                }
+                currentIdx += num; // 최종 위치 업데이트
+            }
         }
 
         // 2번 이상 방문한 영역의 크기 계산
@@ -25,21 +35,5 @@ public class Main {
         }
 
         System.out.println(result);
-    }
-
-    // 경로를 기록하고 현재 위치를 반환하는 함수
-    public static int record(int num, char direction, int[] arr, int currentIdx) {
-        if (direction == 'L') {
-            for (int i = currentIdx - 1; i >= currentIdx - num; i--) {
-                arr[i]++;
-            }
-            currentIdx -= num; // 이동한 최종 위치
-        } else { // 오른쪽 이동
-            for (int i = currentIdx + 1; i <= currentIdx + num; i++) {
-                arr[i]++;
-            }
-            currentIdx += num; // 이동한 최종 위치
-        }
-        return currentIdx; // 최종 위치 반환
     }
 }
